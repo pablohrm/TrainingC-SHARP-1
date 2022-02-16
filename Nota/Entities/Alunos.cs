50d1f993d05aadf0096342b77efedd5ef2467ad8
Pablo Machado <pablo.henriquee1@hotmail.com>
2022-02-16 16:17:26 -0300

    Trainig C-SHARP [1]

---
+++ Nota/Entities/Alunos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nota.Entities
+{
+    class Alunos
+    {
+        public string Name { get; set; }
+        public int FinallyNote { get; set; }
+
+        public Alunos(string name, int finallyNote)
+        {
+            Name = name;
+            FinallyNote = finallyNote;
+        }
+    }
+}
---
+++ Nota/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Nota.Entities;
+
+namespace Nota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alunos> list = new List<Alunos>();
+
+            Console.Write("Informe a quantidade de alunos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Nota final: ");
+                int finallyNote = int.Parse(Console.ReadLine());
+                list.Add(new Alunos(name, finallyNote));
+                if (finallyNote >= 7)
+                {
+                    Console.WriteLine("Parabens você foi aprovado!!!");
+                } else
+                {
+                    Console.WriteLine("Infelizmente você é burro e reprovou!");
+                }
+            }
+        }
+    }
+}
